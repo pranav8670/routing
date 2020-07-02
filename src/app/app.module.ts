81bdac2fd0df4f5ac178f2cc96239bbad41e29a8
pranav <pranav.hiwse7@gmail.com>
2020-07-02 09:01:02 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AboutComponent } from './components/about/about.component';
+
+const routes : Routes=[
+ {path : '' , redirectTo:'/home' , pathMatch: 'full'},
+
+ {path : 'home' , component : HomeComponent},
+ {path : 'about' , component : AboutComponent},
+ {path : 'contact' , component : ContactComponent},
+
+ {path : '**' , component : PagenotfoundComponent},
+ 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContactComponent,
+    PagenotfoundComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
